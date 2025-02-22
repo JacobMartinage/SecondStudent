1b09f77dfc78f26b4c5faa565d6b64bde3bd65e4
Emmet-Ledell <emmet.ledell@gmail.com>
2025-02-22 17:24:03 -0500

    Cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { EllipsisVerticalIcon } from '@heroicons/react/24/solid';
+import {
+  EllipsisVerticalIcon,
+  CircleStackIcon,
+} from '@heroicons/react/24/solid';
 
 const Dashboard: React.FC = () => {
   return (
     <div className=" w-full flex justify-between gap-4">
-      <div className="text-green-500">Dashboard Page</div>
-      <Cards Course="a" Semester="a" />
+      <div className="">Dashboard Page</div>
+      <Cards
+        Course="Computer Systems"
+        Semester="Cs 2025"
+        LongName="CS2222222"
+      />
     </div>
   );
 };
@@ -13,22 +20,30 @@ const Dashboard: React.FC = () => {
 type CardProps = {
   Course: string;
   Semester: string;
+  LongName: string;
 };
-function Cards({ Course, Semester }: CardProps) {
+function Cards({ Course, Semester, LongName }: CardProps) {
   return (
-    <div className="h-[90px] w-[90px]">
-      <div className="rounded-sm h-full w-full border-red-500 bg-white overflow-hidden">
+    <div className="h-96 w-96 shadow-xs">
+      <div className="rounded-lg h-full w-full bg-white overflow-hidden">
         <div className="h-1/2 bg-green-500 relative">
           <EllipsisVerticalIcon
-            width={20}
+            width={50}
             strokeWidth={2}
             className="absolute top-2 right-2"
           />
         </div>
-        <div className="">
-          {Course}
 
-          {Semester}
+        <a href="" className="">
+          <div className="flex flex-col text-black p-2  h-auto w-aut gap-0">
+            <span className="text-green-500">{Course}</span>
+            <span>{LongName}</span>
+            <span>{Semester} </span>
+          </div>
+        </a>
+
+        <div className="text-black">
+          <CircleStackIcon width={20} strokeWidth={2} />
         </div>
       </div>
     </div>
